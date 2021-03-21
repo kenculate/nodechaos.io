8905e6c9d98dbcb950cc08d06dc8877e7fc344ff
kenculate <saeid.gholizade@gmail.com>
2021-03-22 01:10:12 +0800

    fix the wrong files

--- sketch.js
+++ sketch.js
@@ -1,72 +1,108 @@
-const cells = [];
-const connections = [];
-
-
+var nodes = [];
+var selected_node;
+var offsetX;
+var offsetY;
+var mouse_pressX;
+var mouse_pressY;
+var dragging = false;
 function setup() {
-  createCanvas(600, 600);
-  cells.push(new Cell('0'));
-  cells.push(new Cell('1'));
-  
-  connections.push(new Connection(cells[0], cells[1]));
+  createCanvas(800, 600);
+  nodes.push(new Node(100, 10, 150, 150, "node1"));
+  nodes.push(new Node(350, 10, 150, 150, "node2"));
 }
 
 function draw() {
-  background(100);
-  
-  connections.forEach(conn => {
-    if (conn.isInside(mouseX, mouseY)) conn.flags.hover = true;
-    else conn.flags.hover = false;
-    
-    conn.render();
-  })
-  
-  cells.forEach (cell => {
-    if (cell.isInside(mouseX, mouseY)) cell.flags.hover = true;
-    else cell.flags.hover = false;
-    
-    cell.render();
-  });
+  background(220);
+  for (let i=0; i < nodes.length; i++)
+  {
+    nodes[i].is_inside(mouseX, mouseY);
+    nodes[i].render();
+  }
+  if (!selected_node && dragging)
+  {
+    noFill();
+    stroke(10);
+    rect(mouse_pressX, mouse_pressY, mouseX - mouse_pressX, mouseY - mouse_pressY);
+  }
 }
 
-
-let dx = 0;
-let dy = 0;
-let dragged_cell;
-
 function mousePressed() {
-  
-  for (let i = 0; i < connections.length; i++) {
-    conn = connections[i];
-    if (conn.flags.hover) {
-      connections.splice(i, 1);
-      return;
-    }
-  }
-  
-  for (let i = 0; i < cells.length; i++) {
-    cell = cells[i];
-    if (cell.flags.hover) {
-      cell.flags.dragging = true;
-      dragged_cell = cell;
+  dragging = true;
+  mouse_pressX = mouseX;
+  mouse_pressY = mouseY;
+  for (let i=nodes.length-1; i >= 0; i--)
+  {
+    if (nodes[i].is_pressed())
+    {
+      selected_node = nodes[i];
+      offsetX = mouseX - nodes[i].x;
+      offsetY = mouseY - nodes[i].y;
       break;
     }
   }
   
-  if (!dragged_cell) return;
-  dx = mouseX - dragged_cell.x;
-  dy = mouseY - dragged_cell.y;
 }
 
 function mouseDragged() {
-  if (!dragged_cell) return;
-  
-  dragged_cell.x = mouseX - dx;
-  dragged_cell.y = mouseY - dy;
+  if (selected_node)
+  {
+    selected_node.x = mouseX - offsetX;
+    selected_node.y = mouseY - offsetY;
+  }
 }
 
 function mouseReleased() {
-  if (!dragged_cell) return;
+  dragging = false;
   
-  dragged_cell.flags.dragging = false;
-  dragged_cell = undefined;
+  if (selected_node != undefined)
+  {
+    selected_node.pressed = false;
+  }
+  selected_node = undefined;
+}
+
+class Node{
+  constructor(x, y, w, h, title)
+  {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.title = title;
+    this.hover = false;
+    this.pressed = false;
+    this.title_height = 20;
+    this.knob_size = 20;
+  }
+  
+  render(){
+    stroke(150);
+    fill(this.pressed ? 150 : this.hover ? 100 : 50);
+    rect(this.x, this.y, this.w, this.h);
+    
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text(this.title, this.x + 5, this.y, this.w - 5, this.title_height);
+    line(this.x, this.y + this.title_height, this.x + this.w, this.y + this.title_height);
+    
+    noStroke();
+    fill(170);
+    rect(this.x - this.knob_size, this.y + this.title_height + this.knob_size, this.knob_size, this.knob_size);
+    fill(120);
+    
+    rect(this.x + this.w, this.y + this.title_height + this.knob_size, this.knob_size, this.knob_size);
+  }
+  
+  is_pressed()
+  {
+    this.pressed = this.hover;
+    return this.pressed;
+  }
+  
+  is_inside(x, y)
+  {
+    this.hover = (x > this.x && x < this.x + this.w &&
+        y > this.y && y < this.y + this.h);
+    return this.hover;
+  }
 }
